2e64e8b86c21b2d5ab9d627df3e017b33c9678b5
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2024-11-25 16:24:58 +0600

    DZ_Sem7

---
+++ src/main/java/ru/geekbrains/Spring_Sem7/SpringSem7Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.Spring_Sem7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSem7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSem7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/Spring_Sem7/SpringSem7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.Spring_Sem7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSem7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
